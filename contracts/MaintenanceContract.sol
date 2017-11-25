d8e31e988ce726f857a2bb61f6414b63936de212
Niklas <eraco@live.de>
2017-11-25 18:29:03 +0100

    add ownership functionality to MeintenanceContract
    add ownershit functionalitz to MeintenanceContract

--- contracts/MaintenanceContract.sol
+++ contracts/MaintenanceContract.sol
@@ -11,17 +11,19 @@ contract MaintenanceContract {
         uint totalUsedHours;
         uint hoursSinceLastInspection;
         bool active;
+        address owner;
+
     }
 
     mapping (uint => Device) public devices;
 
     //erzeugt ein neues Geraet, ID muss eindeutig sein und darf vorher nicht existieren
     function addDevice(uint deviceId, string deviceName) public {
-        devices[deviceId] = Device(deviceName,0,0,true);
+        devices[deviceId] = Device(deviceName,0,0,true,msg.sender);
     }
 
     function bookUsedHours(uint deviceId, uint hoursUsed) public returns (uint newTotalUsedHours) {
-        Device d = devices[deviceId];
+        Device storage d = devices[deviceId];
         d.totalUsedHours = d.totalUsedHours + hoursUsed;
         d.hoursSinceLastInspection = d.hoursSinceLastInspection + hoursUsed;
         //devices[deviceId] = d;
@@ -40,4 +42,16 @@ contract MaintenanceContract {
         devices[deviceId].active = false;
     }
 
+    //uebertraegt ownership an neue adresse, sender muss aktueller owner sein
+    function transferOwnership(uint deviceId, address newOwner) public {
+        if (devices[deviceId].owner == msg.sender) {
+            if (newOwner != 0x0) {
+              devices[deviceId].owner = newOwner;  
+            }
+        }
+    }
+
+    function getCurrentOwner(uint deviceId) public returns(address) {
+        return devices[deviceId].owner;
+    }
 }
--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,4 @@
-const Web3 = require('web3');
+//const Web3 = require('web3');
 
 const TruffleConfig = require('../truffle');
 
@@ -8,7 +8,7 @@ var Migrations = artifacts.require("./Migrations.sol");
 module.exports = function(deployer, network, addresses) {
   const config = TruffleConfig.networks[network];
 
-  const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
+  //const web3 = new Web3(new Web3.providers.HttpProvider('http://' + config.host + ':' + config.port));
 
   console.log('>> Unlocking account ' + config.from);
   //web3.personal.unlockAccount(config.from, 'TEST', 36000);
