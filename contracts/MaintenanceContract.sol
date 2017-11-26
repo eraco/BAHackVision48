8dbb6c5d7d8dd95a48f6e97ad0f44f338ed2d1f0
Stephan Scheide <privat@kingster.de>
2017-11-26 11:44:23 +0100

    Fixes

---
+++ contracts/MaintenanceContract.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.17;
+
+/*
+* Vertrag fuer eine Menge an Geraeten
+*/
+contract MaintenanceContract {
+
+    //Entitaet: Unsere Geraet
+    struct Device {
+        string name;
+        uint totalUsedHours;
+        uint hoursSinceLastInspection;
+        bool active;
+        //address owner;
+    }
+
+    mapping (uint => Device) public devices;
+
+    //erzeugt ein neues Geraet, ID muss eindeutig sein und darf vorher nicht existieren
+    function addDevice(uint deviceId, string deviceName) public {
+        devices[deviceId] = Device(deviceName,0,0,true);
+    }
+
+    function bookUsedHours(uint deviceId, uint hoursUsed) public returns (uint) {
+        Device storage d = devices[deviceId];
+        d.totalUsedHours = d.totalUsedHours + hoursUsed;
+        d.hoursSinceLastInspection = d.hoursSinceLastInspection + hoursUsed;
+        //devices[deviceId] = d;
+        return d.totalUsedHours;
+    }
+
+    function getHoursSinceLastInspectionById(uint deviceId) public returns (uint) {
+        uint  hrs = devices[deviceId].hoursSinceLastInspection;
+        return hrs;
+    }
+
+    function resetInspectionHoursOfDevice(uint deviceId) public {
+        devices[deviceId].hoursSinceLastInspection = 0;
+    }
+
+    /*function getDeviceById(uint deviceId) public returns(Device) {
+        return devices[deviceId];
+    }*/
+
+    /*function getTotalUsedHoursById(uint deviceId) public returns (uint totalUsedHours) {
+        return devices[deviceId].totalUsedHours; 
+    }*/
+
+    function deactivateDeviceById(uint deviceId) public {
+        devices[deviceId].active = false;
+    }
+
+    //uebertraegt ownership an neue adresse, sender muss aktueller owner sein
+    /*function transferOwnership(uint deviceId, address newOwner) public {
+        if (devices[deviceId].owner == msg.sender) {
+            if (newOwner != 0x0) {
+              devices[deviceId].owner = newOwner;  
+            }
+        }
+    }
+
+    function getCurrentOwner(uint deviceId) public returns(address) {
+        return devices[deviceId].owner;
+    }*/
+}
--- web3connector/index.js
+++ web3connector/index.js
@@ -12,7 +12,8 @@ client.setProvider(provider);
 
 var accountId = "0x94734e97f6f627619d1fa7ea5935925e2e08b7dd";
 
-var fc  = new client.eth.Contract(abiFridge,'0x38478939c083ea0723813988ff52418c4be4a5bf');
+var fc  = new client.eth.Contract(abiFridge,'0xf9a4d3c061e692b5fe552e96ee820e8051e7fc30');
+var dm = new client.eth.Contract(abiMaintenance,'0xabe5ba1679b30b5e9874bacc5844b93eda1e72b8');
 
 function sendJsonAnswer(res,str) {
     res.setHeader('Content-Type', 'application/json');
@@ -22,6 +23,13 @@ function sendJsonAnswer(res,str) {
 function sendJsonObject(res,o) {
     sendJsonAnswer(res,JSON.stringify(o));
 }
+
+/*var promise = dm.methods.addDevice(1,"Handy").call();
+promise.then(_d);
+
+promise = dm.methods.deactivateDeviceById(1).call();
+promise.then(_d);*/
+
 
 var serv = Exp();
 serv.get('/givecola/:id', function(req, res) {
@@ -36,7 +44,17 @@ serv.get('/givecola/:id', function(req, res) {
 serv.get('/test', function(req, res) {
     sendJsonObject(res,{test:true});
 });
-    
+
+
+serv.get('/addDevice/:id/:name', function(req, res) {
+    var promise = dm.methods.addDevice(req.params.id,req.params.name).call();
+ 
+    promise.then( (result) => {
+        sendJsonObject(res,{ownerAddress:result});
+    });
+   
+});
+
 _d("server is running");
 serv.listen(3000);
 
